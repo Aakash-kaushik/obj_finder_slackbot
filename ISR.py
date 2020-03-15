abbdee095f79f9c7052fb91cb0d4f12d80fe135e
Kunal Mundada <53429438+AlKun25@users.noreply.github.com>
2020-03-15 12:46:30 +0530

    Added another module
    The module 'frameEnhancer' is used to improvise and apply Image Super Resolution on the input to the model.

--- ISR.py
+++ ISR.py
@@ -1,14 +1,28 @@
 import numpy as np
 from PIL import Image
 from ISR.models import RDN, RRDN
+import cv2
 
-def SuperRes(lr_img):
+def SuperRes(name):# changes the output shown to the user/client
+  img = Image.open('name')
+  lr_img = np.array(img)
   rdn = RDN(weights='noise-cancel')
   sr_img = rdn.predict(lr_img)
   rrdn = RRDN(weights='gans')
   lr_img = rrdn.predict(sr_img)
   sr_img = rdn.predict(lr_img)
-  Image.fromarray(sr_img)
+  cv2.imwrite("name", Image.fromarray(sr_img))
+  
+def frameEnhancer(frame):# changes the input to the model
+  lr_img = np.array(img)
+  rdn = RDN(weights='noise-cancel')
+  sr_img = rdn.predict(lr_img)
+  rrdn = RRDN(weights='gans')
+  lr_img = rrdn.predict(sr_img)
+  sr_img = rdn.predict(lr_img)
+  frame = Image.fromarray(sr_img)
+  return frame
+  
 
 
 
